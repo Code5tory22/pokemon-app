d66666a3efe954827b9e5e86d0430abceb7b5638
brandon gibson <you@example.com>
2023-12-12 02:45:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    const data = getPokemonAPI();
+    setPokemon(data);
+  }, []);
+
+  async function getPokemonAPI() {
+    await fetch("https://pokeapi.co/api/v2/pokemon?limit=15")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+  }
+
+  const nextPage = async () => {
+    await fetch(pokemon.next)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+
+    return (
+      <ul>
+        {pokemon.results &&
+          pokemon.results.map((item) => <Pokemon pokemon={item} />)}
+      </ul>
+    );
+  };
+
+  const prevPage = async () => {
+    await fetch(pokemon.previous)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+
+    return (
+      <ul>
+        {pokemon.results &&
+          pokemon.results.map((item) => <Pokemon pokemon={item} />)}
+      </ul>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <ul>
+          {pokemon.results &&
+            pokemon.results.map((item) => <Pokemon pokemon={item} />)}
+        </ul>
+        {pokemon.previous == null ? null : (
+          <button onClick={prevPage}>Previous</button>
+        )}
+        <button onClick={nextPage}>Next</button>
+      </main>
     </div>
   );
+}
+
+function Pokemon(props) {
+  return <li>{props.pokemon.name}</li>;
 }
 
 export default App;
